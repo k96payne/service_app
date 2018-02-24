01b70ccbad36324d6d829166cc11517c4a777f41
reyjroliva <reyjroliva@ufl.edu>
2018-02-23 22:27:19 -0500

    Updated login UI

--- app/src/main/java/com/group12/service_app/signup_view.java
+++ app/src/main/java/com/group12/service_app/signup_view.java
@@ -1,7 +1,9 @@
 package com.group12.service_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class signup_view extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class signup_view extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_view);
+    }
+
+    public void signup_user(View view){
+        Intent signup_new_user = new Intent(this, login_view.class);
+        startActivity(signup_new_user);
     }
 }
