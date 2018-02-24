4cf5871e8a2e981eda3cbaf8c4c1caf4305beb28
reyjroliva <reyjroliva@ufl.edu>
2018-02-23 22:03:08 -0500

    Login button moves user from login screen to main activity

--- app/src/main/java/com/group12/service_app/login_view.java
+++ app/src/main/java/com/group12/service_app/login_view.java
@@ -1,7 +1,9 @@
 package com.group12.service_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class login_view extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class login_view extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_view);
+    }
+
+    public void login(View view) {
+        Intent user_login = new Intent(this, MainActivity.class);
+        startActivity(user_login);
     }
 }
