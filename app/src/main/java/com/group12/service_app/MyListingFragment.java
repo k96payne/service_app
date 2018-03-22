8642858aca77046d685b29c15e175db63598ccb2
reyjroliva <reyjroliva@ufl.edu>
2018-03-21 20:06:50 -0400

    Added button to MyListings to move to basic listing detail view UI

--- app/src/main/java/com/group12/service_app/MyListingFragment.java
+++ app/src/main/java/com/group12/service_app/MyListingFragment.java
@@ -1,12 +1,15 @@
 package com.group12.service_app;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 
@@ -66,7 +69,18 @@ public class MyListingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_listing, container, false);
+        View view = inflater.inflate(R.layout.fragment_my_listing, container, false);
+
+        Button moveToDetailsBtn = (Button) view.findViewById(R.id.moveToDetailsBtn);
+        moveToDetailsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent moveToDetails = new Intent(getActivity(), listing_details_view.class);
+                startActivity(moveToDetails);
+            }
+        });
+
+        return view;
     }
 
 //    // TODO: Rename method, update argument and hook method into UI event
