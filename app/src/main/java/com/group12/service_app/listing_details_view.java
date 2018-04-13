1ac40a4468bbf0e8c302bb8240b65ab6ac5ffff0
reyjroliva <reyjroliva@ufl.edu>
2018-04-13 19:56:40 -0400

    Almost done implementing listing detail view to show delete/respond button when user moves to listings details view

--- app/src/main/java/com/group12/service_app/listing_details_view.java
+++ app/src/main/java/com/group12/service_app/listing_details_view.java
@@ -3,10 +3,25 @@ package com.group12.service_app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.group12.service_app.core.repositories.ListingRepository;
+import com.group12.service_app.core.repositories.UserRepository;
 import com.group12.service_app.data.models.Listing;
+import com.group12.service_app.data.models.UserPreferences;
+
+import java.util.Iterator;
 
 public class listing_details_view extends AppCompatActivity {
 
@@ -18,6 +33,7 @@ public class listing_details_view extends AppCompatActivity {
     private TextView priceTextView;
     private TextView addressTextView;
     private TextView descriptionTextView;
+    private Button listingResponseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +46,14 @@ public class listing_details_view extends AppCompatActivity {
         this.priceTextView = (TextView) findViewById(R.id.payoffPriceLabel);
         this.addressTextView = (TextView) findViewById(R.id.dueAddressLabel);
         this.descriptionTextView = (TextView) findViewById(R.id.dueDescriptionLabel);
+        this.listingResponseButton = (Button) findViewById(R.id.respondToListingButton);
 
         Listing listing = (Listing)getIntent().getSerializableExtra("listing");
         String title = getIntent().getStringExtra("title");
         String description = getIntent().getStringExtra("description");
         String zipCode = getIntent().getStringExtra("address");
         Double price = getIntent().getDoubleExtra("price", 0);
+        final String listingID = getIntent().getStringExtra("lisitngID");
 
 
         if(listing != null) {
@@ -44,10 +62,31 @@ public class listing_details_view extends AppCompatActivity {
         else
         {
             this.listingTitleTextView.setText(title);
-            this.priceTextView.setText("$" + price);
+            this.priceTextView.setText("$" + price.toString());
             this.addressTextView.setText(zipCode);
             this.descriptionTextView.setText(description);
         }
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference listingsReference = database.getReference("listings");
+        //final Query usersListings = listingsReference.orderByChild("ownerId").equalTo(user.getUid());
+        final Query listingsMatchingId = listingsReference.orderByChild("id").equalTo(listingID);
+        listingsMatchingId.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                throw databaseError.toException();
+            }
+        });
+
+
     }
 
     public void LoadListinDetails(Listing listing) {
